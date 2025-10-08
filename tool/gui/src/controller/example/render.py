0589aa0220ad6558030f5d15455ac9b13271b319
EmaChini <lele.chini@gmail.com>
2025-10-08 19:13:19 +0200

    Added tool gui stable

---
+++ tool/gui/src/controller/example/render.py
@@ -0,0 +1,31 @@
+from dash import Output, Input, dcc
+from gui.src.model.bpmn import validate_bpmn_dict
+from gui.src.model.etl import load_bpmn_dot
+from gui.src.view.visualizer.RenderSVG import RenderSvg
+
+
+def register_example_callbacks(callback, id, example_path):
+	RenderSvg.register_callbacks(callback, id + "-svg")
+
+	@callback(
+		Output(id + "-download", "data"),
+		Input(id + "-download-btn", "n_clicks"),
+		prevent_initial_call=True
+	)
+	def download_bpmn_example(n_clicks):
+		with open(example_path, "r") as file:
+			content = file.read()
+		return dcc.send_string(
+			content,
+			filename="example_bpmn.json"
+		)
+
+
+def render_example(data):
+	bpmn = validate_bpmn_dict(data.get("bpmn", {}))
+	try:
+		bpmn_dot = load_bpmn_dot(bpmn)
+	except Exception as exception:
+		raise RuntimeError(f"Failed to load BPMN dot: {exception}")
+
+	return bpmn, bpmn_dot
---
+++ tool/src/paco/evaluations/evaluate_impacts.py
@@ -0,0 +1,42 @@
+import copy
+import numpy as np
+from src.paco.saturate_execution.states import States, ActivityState
+from src.paco.parser.parse_node import ParseNode, Sequential, Parallel, ExclusiveGateway, Nature, Task, Gateway
+
+
+def evaluate_expected_impacts(states: States, impacts_size: int) -> (np.float64, np.ndarray):
+	impacts = np.zeros(impacts_size, dtype=np.float64)
+	probability = np.float64(1.0)
+
+	for node, state in states.activityState.items():
+		if (isinstance(node, Nature) and state > ActivityState.WAITING
+				and ((node.sx_child in states.activityState and states.activityState[node.sx_child] > ActivityState.WAITING)
+					 or (node.dx_child in states.activityState and states.activityState[node.dx_child] > ActivityState.WAITING))):
+
+			p = node.probability
+			if states.activityState[node.dx_child] > 0:
+				p = 1 - p
+			probability *= p
+
+		elif isinstance(node,Task) and state > ActivityState.WAITING:
+			impacts += node.impact
+
+	return probability, impacts
+
+def evaluate_expected_impacts_from_parseNode(parseNode: ParseNode, impacts_size: int) -> (np.float64, np.ndarray):
+	probability = np.float64(1)
+
+	if isinstance(parseNode, Task):
+		return parseNode.impact
+
+	expected_impacts = np.zeros(impacts_size, dtype=np.float64)
+	if isinstance(parseNode, Gateway):
+		sx_expected_impacts = evaluate_expected_impacts_from_parseNode(parseNode.sx_child, impacts_size)
+		dx_expected_impacts = evaluate_expected_impacts_from_parseNode(parseNode.dx_child, impacts_size)
+		if isinstance(parseNode, Nature):
+			sx_expected_impacts *= parseNode.probability
+			dx_expected_impacts *= (1 - parseNode.probability)
+
+		expected_impacts = sx_expected_impacts + dx_expected_impacts
+
+	return expected_impacts
